8137f7fd4f7355922cf81c49f731c8869c05839d
marco238 <marco238cuba@gmail.com>
2018-05-26 19:55:22 +0200

    feat: sphere with ligth

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
 }
-
 function draw() {
+  background(50, 50, 50);
+  //move your mouse to change light position
+  var locX = mouseX - width / 2;
+  var locY = mouseY - height / 2;
 
+  pointLight(255, 255, 255, locX, locY, 50);
+  ambientMaterial(255);
+  noStroke();
+  sphere(35);
 }
