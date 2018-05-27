6fbbd74c9119cb5f80bd8bfcab3d1fb795308f26
marco238 <marco238cuba@gmail.com>
2018-05-27 21:17:03 +0200

    feat: slider to control angles

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
+var angle;
+
 function setup() {
+  var slider = document.getElementById('slider');
+  angle = PI / 4;
   createCanvas(windowWidth, windowHeight);
 }
+
 function draw() {
   background(51);
+  angle = slider.value;
+
   translate(width / 2, height);
   stroke(255);
   branch(300)
@@ -11,7 +18,7 @@ function draw() {
 function branch(len) {
   line(0, 0, 0, - len);
   translate(0, -len);
-  rotate(PI / 4);
+  rotate(angle);
   line(0, 0, 0, - len*0.67);
   if (len > 2) {
     branch(len*0.67);
